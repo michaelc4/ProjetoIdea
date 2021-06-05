40bc09a731e453a7b9dd7cffba3a4c3ae99656e5
Michael Censi <michael.c@lyncas.net>
2021-06-05 17:09:19 -0300

    fix bug

---
+++ Front/src/app/components/auth/menu_administrativo/modal/modal_voluntarios.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-modal-voluntarios',
+  templateUrl: `modal_voluntarios.component.html`,
+  styleUrls: ['modal_voluntarios.component.scss'],
+  providers: []
+})
+export class MenuAdministrativoComponent {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
